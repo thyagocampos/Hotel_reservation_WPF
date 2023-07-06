22a5bae64497f04a20e3b56d34bafcfeae052ad8
Thyago <thyagosic@gmail.com>
2023-07-06 13:07:57 -0300

    Finish validation using INotifyDataErrorInfo

--- WpfApp1/ViewModels/MakeReservationViewModel.cs
+++ WpfApp1/ViewModels/MakeReservationViewModel.cs
@@ -1,17 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using WpfApp1.Commands;
-using WpfApp1.Models;
 using WpfApp1.Services;
 using WpfApp1.Stores;
 
 namespace WpfApp1.ViewModels
 {
-    public class MakeReservationViewModel : ViewModelBase
+    public class MakeReservationViewModel : ViewModelBase, INotifyDataErrorInfo
     {
         private string _userName = "";
         private string _roomNumber = "";
@@ -59,7 +58,17 @@ namespace WpfApp1.ViewModels
             set
             {
                 _startDate = value;
+
                 OnPropertyChanged(nameof(StartDate));
+                
+                ClearErrors(nameof(StartDate));
+                ClearErrors(nameof(EndDate));
+
+                if (EndDate < StartDate)
+                {
+                    AddError("The start date cannot be after the end date.", nameof(StartDate));
+                }
+
             }
         }
 
@@ -70,10 +79,19 @@ namespace WpfApp1.ViewModels
             set
             {
                 _endDate = value;
+
                 OnPropertyChanged(nameof(EndDate));
+                
+                ClearErrors(nameof(StartDate));
+                ClearErrors(nameof(EndDate));
+
+                if (EndDate < StartDate)
+                {
+                    AddError("The end date cannot be before the start date.", nameof(EndDate));                    
+                }
             }
         }
-
+       
         public ICommand? SubmitCommand { get; } 
 
         public ICommand? CancelCommand { get; }
@@ -82,6 +100,52 @@ namespace WpfApp1.ViewModels
         {
             SubmitCommand = new MakeReservationCommand(this, hotelStore, reservationViewNavigationService);
             CancelCommand = new NavigateCommand(reservationViewNavigationService);
+
+            _propertyNameToErrorDictionary = new Dictionary<string, List<string>>();
+        }
+
+        //INotifyDataErrorInfo errors memebers
+
+        private readonly Dictionary<string, List<string>> _propertyNameToErrorDictionary;
+
+        public bool HasErrors
+        {
+            get
+            {
+                return _propertyNameToErrorDictionary.Any();
+
+            }
+        } 
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            return _propertyNameToErrorDictionary.GetValueOrDefault(propertyName, new List<string>());
+        }
+
+        private void ClearErrors(string propertyName)
+        {         
+            _propertyNameToErrorDictionary.Remove(propertyName);
+            
+            OnErrorsChange(propertyName);
+        }
+
+        private void OnErrorsChange(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        private void AddError(string errorMessage, string propertyName)
+        {
+            if(!_propertyNameToErrorDictionary.ContainsKey(propertyName))
+            {
+                _propertyNameToErrorDictionary.Add(propertyName, new List<string>());
+            }
+
+            _propertyNameToErrorDictionary[propertyName].Add(errorMessage);
+
+            OnErrorsChange(propertyName);
         }
     }
 }
